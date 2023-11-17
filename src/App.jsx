da3f76f6a0a25eb8f399b4db1851e048d28d6aaf
u1yuvan <yuvanraj700@gmail.com>
2023-11-17 20:29:42 +0530

    Structure has been created Adding some basic styles

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ const App = () => (
     <section className="padding-x sm:py-32 py-16 w-full">
       Subscribe
     </section>
-    <section className="padding">
+    <section className="padding bg-black padding-x padding-t pb-8">
       Footer
     </section>
     <section className="padding">
