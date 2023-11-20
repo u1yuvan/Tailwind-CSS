7c6d2ac5e86391c69399b3e39cd8bfd61e7557c9
u1yuvan <yuvanraj700@gmail.com>
2023-11-20 20:58:37 +0530

    added tailwind stylings for Nav bar and hero section

---
+++ src/sections/index.js
@@ -0,0 +1,21 @@
+import CustomerReviews from './CustomerReviews';
+import Hero from './Hero';
+import PopularProducts from './PopularProducts';
+import Services from './Services';
+import SpecialOffer from './SpecialOffer';
+import Subscribe from './Subscribe';
+import SuperQuality from './SuperQuality';
+import Footer from './Footer';
+
+
+
+export {
+    Hero,
+    CustomerReviews,
+    PopularProducts,
+    Subscribe,
+    Services,
+    SpecialOffer,
+    SuperQuality,
+    Footer,
+}
