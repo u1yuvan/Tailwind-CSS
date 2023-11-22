c149fb9b1d50b67ec8357b8f67213a13be3fd4a6
u1yuvan <yuvanraj700@gmail.com>
2023-11-22 21:01:57 +0530

    Popular Products and Super Quality section has been implemented

--- src/constants/index.js
+++ src/constants/index.js
@@ -33,23 +33,23 @@ export const statistics = [
 export const products = [
     {
         imgURL: shoe4,
-        name: "Nike Air Jordan-01",
-        price: "$200.20",
+        name: "Nike Sport 01",
+        price: "₹1899",
     },
     {
         imgURL: shoe5,
-        name: "Nike Air Jordan-10",
-        price: "$210.20",
+        name: "Nike k-Swiss 2023",
+        price: "₹2999",
     },
     {
         imgURL: shoe6,
-        name: "Nike Air Jordan-100",
-        price: "$220.20",
+        name: "Nike Air 100",
+        price: "₹2101",
     },
     {
         imgURL: shoe7,
-        name: "Nike Air Jordan-001",
-        price: "$230.20",
+        name: "Nike AirFlex 0026",
+        price: "₹3559",
     },
 ];
 
